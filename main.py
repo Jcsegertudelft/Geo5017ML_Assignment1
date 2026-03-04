477ada1ec3fb3ee75a67fc4719200d1d4f4285f6
Jcsegertudelft <jcsegers@tudelft.nl>
2026-03-04 15:22:30 +0100

    Minor alterations for easier adjustment of parameters

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 '''
-MAIN FILE
-
-WORK ON COMPONENTS IN SEPERATE FILES
+Code Group 10: Job Segers, Timber Groeneveld, Akhil Veeranki
 '''
-
 
 import numpy as np
 from  matplotlib import pyplot as plt
 from Models import constv,consta
 
+Model_used = 'constv' #set to constv for constant velocity, to consta for constant acceleration
+Learning_rate = 0.001
+Max_iter = 100000
+Tolerance = 1e-6 #Minimum step size for another iteration to take place
+Viz = True #Do you want the plot at the end. True or False
 
 def sse(model,positions,time): #sum of squared errors
     pred=np.array([model.value(t) for t in time])
@@ -35,11 +37,11 @@ def create_model(model_type):
         ma=consta(a=[0.0,0.0,0.0],r0=[0.0,0.0,0.0], v0=[0.0,0.0,0.0])
         return ma
     else:
-        print("Model type not recognized")
+        raise Exception("Model type not recognized, input 'constv' or 'consta'")
         return None
 
 
-def main():
+def main(Viz):
     # Define the time points and positions
     T=np.array([1,2,3,4,5,6], dtype=float)
     P=np.array([
@@ -52,9 +54,9 @@ def main():
         dtype=float)
 
     # Create the model with either constv or consta
-    model = create_model("consta")
+    model = create_model(Model_used)
     # Train the model using gradient descent
-    lr_gradient_descent = gradient_descent(model,P,T,learn_rate=0.001,max_iter=100000,tol=0.000001)
+    lr_gradient_descent = gradient_descent(model,P,T,learn_rate=Learning_rate,max_iter=Max_iter,tol=Tolerance)
     # Print the parameters
     gd_output = lr_gradient_descent.return_vars()
     rounded_gd_output = [np.round(arr, 4) for arr in gd_output]
@@ -77,30 +79,31 @@ def main():
     z_measured = P[:,2]
 
     # Create the 3D figure with the tracked positions and the trajectory from gradient descent
-    fig = plt.figure(figsize=(8,6))
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(x_measured,y_measured,z_measured, color='navy', label='Measured Positions')
-    ax.scatter(x_predicted[-1],y_predicted[-1],z_predicted[-1], color='navy', label='Predicted Position at t=7', s=80)
-    ax.plot(x_predicted,y_predicted,z_predicted, linewidth=1.4, color='crimson', label='Estimated Trajectory')
-    ax.set_xlabel('X Position')
-    ax.set_ylabel('Y Position')
-    ax.set_zlabel('Z Position')
-    ax.set_title('Estimated Drone Trajectory using Gradient Descent', pad=20)
-    ax.text(x_measured[0]+ 0.2,y_measured[0],z_measured[0],'t = 1')
-    ax.text(x_measured[1]+ 0.2,y_measured[1],z_measured[1],'t = 2')
-    ax.text(x_measured[2]+ 0.2,y_measured[2],z_measured[2],'t = 3')
-    ax.text(x_measured[3]+ 0.2,y_measured[3],z_measured[3],'t = 4')
-    ax.text(x_measured[4]+ 0.2,y_measured[4],z_measured[4],'t = 5')
-    ax.text(x_measured[5]+ 0.2,y_measured[5],z_measured[5],'t = 6')
-    ax.text(x_predicted[-1]+ 0.35,y_predicted[-1],z_predicted[-1],'t = 7')
-    ax.set_xlim(-3, 3)
-    ax.set_ylim(-5, 2)
-    ax.set_aspect('equal')
-    ax.legend()
-    plt.tight_layout()
-    plt.savefig('Drone Trajectory.png', dpi = 300)
-    plt.show()
+    if Viz:
+        fig = plt.figure(figsize=(8,6))
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(x_measured,y_measured,z_measured, color='navy', label='Measured Positions')
+        ax.scatter(x_predicted[-1],y_predicted[-1],z_predicted[-1], color='navy', label='Predicted Position at t=7', s=80)
+        ax.plot(x_predicted,y_predicted,z_predicted, linewidth=1.4, color='crimson', label='Estimated Trajectory')
+        ax.set_xlabel('X Position')
+        ax.set_ylabel('Y Position')
+        ax.set_zlabel('Z Position')
+        ax.set_title('Estimated Drone Trajectory using Gradient Descent', pad=20)
+        ax.text(x_measured[0]+ 0.2,y_measured[0],z_measured[0],'t = 1')
+        ax.text(x_measured[1]+ 0.2,y_measured[1],z_measured[1],'t = 2')
+        ax.text(x_measured[2]+ 0.2,y_measured[2],z_measured[2],'t = 3')
+        ax.text(x_measured[3]+ 0.2,y_measured[3],z_measured[3],'t = 4')
+        ax.text(x_measured[4]+ 0.2,y_measured[4],z_measured[4],'t = 5')
+        ax.text(x_measured[5]+ 0.2,y_measured[5],z_measured[5],'t = 6')
+        ax.text(x_predicted[-1]+ 0.35,y_predicted[-1],z_predicted[-1],'t = 7')
+        ax.set_xlim(-3, 3)
+        ax.set_ylim(-5, 2)
+        ax.set_aspect('equal')
+        ax.legend()
+        plt.tight_layout()
+        plt.savefig('Drone Trajectory.png', dpi = 300)
+        plt.show()
 
 
 if __name__=='__main__':
-    main()
+    main(Viz)
